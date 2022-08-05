063379e630d9415bf74c09840a0268e33ca063da
mansi <kachhiyamanshi15042810@gmail.com>
2022-08-05 15:10:49 +0530

    initial commit

---
+++ src/test/java/io/tntra/demospringboot/DemospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tntra.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
